efcd1aad22ba61d5faa0e3fdb42fdeb65a0cc398
qnnp-me <wujipeng@foxmail.com>
2023-03-20 19:37:10 +0800

    其他

--- .tools/pipline-publish.sh
+++
@@ -1,5 +0,0 @@
-ls -ahl
-git ls-files | while read file; do touch -d $(git log -1 --format="@%ct" "$file") "$file"; done
-ls -ahl
-mkdir doc
-cp -rfp _media en zh-cn _coverpage.md index.html _404.md README.md _common doc
